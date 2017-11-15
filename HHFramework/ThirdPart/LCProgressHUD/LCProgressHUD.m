39e2de2849d3a21d6d3ba3840dc0b875f1aab086
calary <cao229336009@qq.com>
2017-11-15 11:33:52 +0800

    HUD优化

--- HHFramework/ThirdPart/LCProgressHUD/LCProgressHUD.m
+++ HHFramework/ThirdPart/LCProgressHUD/LCProgressHUD.m
@@ -9,6 +9,7 @@
 #define BGVIEW_WIDTH 100.0f
 // 文字大小
 #define TEXT_SIZE    14.0f
+#define MESSAGE_SIZE 16.0f
 
 #define kToastDuration 1.0f
 
@@ -26,98 +27,108 @@
 }
 
 + (void)showStatus:(LCProgressHUDStatus)status text:(NSString *)text {
+    [self showStatus:status text:text inKeyWindow:NO];
+}
 
++ (void)showMessage:(NSString *)text{
+    [self showMessage:text inKeyWindow:NO];
+}
+
++ (void)showStatus:(LCProgressHUDStatus)status text:(NSString *)text inKeyWindow:(BOOL)inKey{
     LCProgressHUD *hud = [LCProgressHUD sharedHUD];
     [hud showAnimated:YES];
     [hud setShowNow:YES];
     //View的颜色
-    hud.bezelView.color = HEXACOLOR(0x000000,0.7);
+    hud.bezelView.color = [UIColor colorWithRed:0 green:0 blue:0 alpha:0.7];
     //style -blur 不透明 －solidColor 透明
     hud.bezelView.style = MBProgressHUDBackgroundStyleSolidColor;
     //HUD内部颜色
-    hud.contentColor = [UIColor colorWithRed:0.82 green:0.82 blue:0.82 alpha:1.0];
-    hud.animationType = MBProgressHUDAnimationFade;
+    //    hud.contentColor = [UIColor colorWithRed:0.82 green:0.82 blue:0.82 alpha:1.0];
+    hud.contentColor = [UIColor whiteColor];
+    hud.animationType = MBProgressHUDAnimationZoom;
     
     hud.label.text = text;
     hud.label.font = [UIFont boldSystemFontOfSize:TEXT_SIZE];
     [hud setRemoveFromSuperViewOnHide:YES];
     [hud setMinSize:CGSizeMake(BGVIEW_WIDTH, BGVIEW_WIDTH)];
-    
-    [[self currentView] addSubview:hud];//添加到当前View
-    
-    hud.offset = CGPointMake(0, -64);//偏移
+    if (inKey){
+        [[UIApplication sharedApplication].keyWindow addSubview:hud];
+    }else{
+        [[self currentView] addSubview:hud];//添加到当前View
+    }
     
     NSString *bundlePath = [[NSBundle mainBundle] pathForResource:@"LCProgressHUD" ofType:@"bundle"];
-
+    
     switch (status) {
-
+            
         case LCProgressHUDStatusSuccess: {
-
+            
             NSString *sucPath = [bundlePath stringByAppendingPathComponent:@"hud_success@2x.png"];
             UIImage *sucImage = [UIImage imageWithContentsOfFile:sucPath];
-
+            
             hud.mode = MBProgressHUDModeCustomView;
             UIImageView *sucView = [[UIImageView alloc] initWithImage:sucImage];
             hud.customView = sucView;
             [hud hideAnimated:YES afterDelay:kToastDuration];
         }
             break;
-
+            
         case LCProgressHUDStatusError: {
-
+            
             NSString *errPath = [bundlePath stringByAppendingPathComponent:@"hud_error@2x.png"];
             UIImage *errImage = [UIImage imageWithContentsOfFile:errPath];
-
+            
             hud.mode = MBProgressHUDModeCustomView;
             UIImageView *errView = [[UIImageView alloc] initWithImage:errImage];
             hud.customView = errView;
             [hud hideAnimated:YES afterDelay:kToastDuration];
         }
             break;
-
+            
         case LCProgressHUDStatusWaitting: {
-
             hud.mode = MBProgressHUDModeIndeterminate;
         }
             break;
-
+            
         case LCProgressHUDStatusInfo: {
-
+            
             NSString *infoPath = [bundlePath stringByAppendingPathComponent:@"hud_info@2x.png"];
             UIImage *infoImage = [UIImage imageWithContentsOfFile:infoPath];
-
+            
             hud.mode = MBProgressHUDModeCustomView;
             UIImageView *infoView = [[UIImageView alloc] initWithImage:infoImage];
             hud.customView = infoView;
             [hud hideAnimated:YES afterDelay:kToastDuration];
         }
             break;
-
+            
         default:
             break;
     }
 }
 
-+ (void)showMessage:(NSString *)text {
++ (void)showMessage:(NSString *)text inKeyWindow:(BOOL)inKey{
 
     LCProgressHUD *hud = [LCProgressHUD sharedHUD];
     [hud showAnimated:YES];
     [hud setShowNow:YES];
-    hud.bezelView.color = HEXACOLOR(0x000000,0.7);
+    hud.bezelView.color = [UIColor colorWithRed:0 green:0 blue:0 alpha:0.7];
     //style -blur 不透明 －solidColor 透明
     hud.bezelView.style = MBProgressHUDBackgroundStyleSolidColor;
     hud.animationType = MBProgressHUDAnimationZoom;
-    hud.contentColor = [UIColor colorWithRed:0.82 green:0.82 blue:0.82 alpha:1.0];
+//    hud.contentColor = [UIColor colorWithRed:0.82 green:0.82 blue:0.82 alpha:1.0];
+    hud.contentColor = [UIColor whiteColor];
     hud.label.text = text;
-    hud.label.font = [UIFont boldSystemFontOfSize:TEXT_SIZE];
+    hud.label.font = [UIFont boldSystemFontOfSize:MESSAGE_SIZE];
     [hud setMinSize:CGSizeZero];
     [hud setMode:MBProgressHUDModeText];
     [hud setRemoveFromSuperViewOnHide:YES];
-    [[self currentView] addSubview:hud];//添加到当前View
+    if (inKey){
+        [[UIApplication sharedApplication].keyWindow addSubview:hud];
+    }else{
+        [[self currentView] addSubview:hud];//添加到当前View
+    }
 
-    //因为全局的ViewController中的View都下沉64
-    hud.offset = CGPointMake(0, -64);
-    
     [hud hideAnimated:YES afterDelay:kToastDuration];
 }
 
@@ -131,14 +142,29 @@
     [self showStatus:LCProgressHUDStatusError text:text];
 }
 
++ (void)showKeyWindowFailure:(NSString *)text {
+    
+    [self showStatus:LCProgressHUDStatusError text:text inKeyWindow:YES];
+}
+
 + (void)showSuccess:(NSString *)text {
 
     [self showStatus:LCProgressHUDStatusSuccess text:text];
 }
 
++ (void)showKeyWindowSuccess:(NSString *)text {
+    
+    [self showStatus:LCProgressHUDStatusSuccess text:text inKeyWindow:YES];
+}
+
+
 + (void)showLoading:(NSString *)text {
 
     [self showStatus:LCProgressHUDStatusWaitting text:text];
+}
+
++ (void)showKeyWindowLoading:(NSString *)text {
+     [self showStatus:LCProgressHUDStatusWaitting text:text inKeyWindow:YES];
 }
 
 + (void)hide {
@@ -151,15 +177,15 @@
     
     if ([controller isKindOfClass:[UITabBarController class]]) {
         controller = [(UITabBarController *)controller selectedViewController];
+        return controller.view;
     }
     if([controller isKindOfClass:[UINavigationController class]]) {
         controller = [(UINavigationController *)controller visibleViewController];
+        return controller.view;
     }
     if (!controller) {
         return [UIApplication sharedApplication].keyWindow;
     }
     return controller.view;
 }
-
-
 @end
